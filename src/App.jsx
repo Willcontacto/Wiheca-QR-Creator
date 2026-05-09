ee8832da63458954973d35802cae7f96ba3e2817
Will <wilsonherreracontacto@gmail.com>
2026-05-09 01:13:35 -0500

    X

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ export default function QRGeneratorApp() {
         {/* Panel izquierdo */}
         <div className="rounded-3xl shadow-xl bg-white p-8 flex flex-col gap-6">
           <div className="flex items-center gap-4">
-  <div className="text-7x1">
+  <div className="text-14x1">
     🟪
   </div>
   <div>
